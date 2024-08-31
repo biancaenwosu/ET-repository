91d887cd39a562044aeb3e61ceae96c20cc27b8f
Ezinwa <bianca.e.nwosu@gmail.com>
2024-08-31 14:59:29 +0100

    template added
    template for adjustment class that may or may not be needed

---
+++ index.java
@@ -0,0 +1,10 @@
+//Introduction page
+
+//class called adjustment 
+class Adjustment{
+    /*every category is an object*/
+    // increase function
+    //decrease function
+    // edit name function
+    // delete function
+}
