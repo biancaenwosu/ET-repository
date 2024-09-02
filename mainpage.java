be26b1087e23aec921225a6b610ee960ee99c9bd
Ezinwa <bianca.e.nwosu@gmail.com>
2024-09-02 18:20:51 +0100

    mainpage

---
+++ mainpage.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.HashMap;
+import javax.swing.*;
+
+public class mainpage {
+    //initialise all the components
+    private JFrame mainframe;
+    private JLabel titleheader;
+    private String username;
+    private String message;
+    private final Color green = new Color(104,119,62,255);
+    private Font Header = new Font("Verdana",Font.BOLD,30);
+    public mainpage(){
+        // get the profile of the user
+        HashMap<String, String> profile = new HashMap<String, String>();
+        profile = DataHandler.returnProfile();
+
+        /*              Header              */ 
+        
+        username = profile.get("Name");
+        message = String.format("Welcome %s", username);
+        titleheader = new JLabel(message);
+        titleheader.setHorizontalAlignment(JLabel.CENTER);
+        titleheader.setFont(Header);
+        titleheader.setBackground(green);
+
+
+
+        // create  the centre of the page with the pie chart
+        
+        // create the mainframe
+        mainframe = new JFrame();
+        // choose the type of layout the main frome will have
+        mainframe.setLayout(new BorderLayout());
+        // dimensions of the main frame
+        mainframe.setSize(400,500);
+        // check if you can see the page
+        mainframe.setVisible(true);
+        // if user clicks 'x' then mainframe closes
+        mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // add things to the mainframe
+        mainframe.add(titleheader, BorderLayout.NORTH);
+
+    
+        
+
+    }
+    public static void main(String[] args) {
+        new mainpage();
+    }
+    
+}
