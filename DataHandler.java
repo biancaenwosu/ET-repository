91e4409e69237faa13129c7b710cfe91f240e55b
MarcWamunuko <mwamunuko@icloud.com>
2024-09-02 21:03:51 +0100

    Comments

--- DataHandler.java
+++ DataHandler.java
@@ -8,8 +8,8 @@ import java.util.HashMap;
 
 public class DataHandler {
     public static void createProfile(String name){
-        HashMap<String,String> profile = new HashMap<String,String>();
-        profile.put("Name", name);
+        HashMap<String,String> profile = new HashMap<String,String>(); // Creates a hashmap where the string value of the key (category) and the value (current price) is stored in pairs
+        profile.put("Name", name);  // Putting in the category and its average value into the hashmap
         profile.put("Rent", "439.0");
         profile.put("Groceries", "133.0");
         profile.put("Household bills", "79.0");
@@ -25,16 +25,16 @@ public class DataHandler {
         profile.put("Gifts and charity", "21.0");
         profile.put("Friends and family", "19.0");
         
-        try {
-            File file = new File("Database");
+        try { // Catching exceptions incase opening fails
+            File file = new File("Database"); // Creating/Opening a file called Database with no filename extension as its unneeded 
             try {
-                FileOutputStream fos = new FileOutputStream(file);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                oos.writeObject(profile);
-                oos.flush();
+                FileOutputStream fos = new FileOutputStream(file); // File Output Stream is used to write binary data from the hashmap 
+                ObjectOutputStream oos = new ObjectOutputStream(fos);   // Works on the bytes of the given object
+                oos.writeObject(profile); // Can write hashmap to the stream as it is serialilizable 
+                oos.flush(); // A buffer which allows all the bytes to be written while the program waits
                 oos.close();
-                fos.close();
-            } catch (IOException i) {
+                fos.close(); // Closing frees up system resources
+            } catch (IOException i) { // IOExceptions are caused by errors in writing or reading files
                 System.out.println("Error writing");
             }
         } catch (Exception x) {
@@ -42,14 +42,14 @@ public class DataHandler {
         }
     }
     public static HashMap<String,String> returnProfile(){
-        HashMap<String,String> profile = new HashMap<String,String>();
+        HashMap<String,String> profile = new HashMap<String,String>();  // Creates a hashmap to store the pre existing hashmap
         try {
         File file = new File("Database"); 
         try {
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            profile = (HashMap<String,String> )ois.readObject();
-            System.out.println(profile.toString());
+            FileInputStream fis = new FileInputStream(file); // Reads binary data
+            ObjectInputStream ois = new ObjectInputStream(fis); // Works on taking in the binary data of the object given
+            profile = (HashMap<String,String> )ois.readObject(); // Sets the hashmap profile to the read hashmap from the file
+            System.out.println(profile.toString()); // Outputs the hashmap to check it was received correctly
         } catch (IOException e) {
             System.out.println("Error writing");
         }
@@ -57,7 +57,26 @@ public class DataHandler {
         System.out.println("Error opening");
      }
      
-     return  profile;
+     return  profile; // Returns the previously saved hashmap to be used
+    }
+
+    public static void UpdateValue(String valueName, String value, HashMap<String,String> profile){
+        profile.replace(valueName, value);
+        try { // Catching exceptions incase opening fails
+            File file = new File("Database"); // Creating/Opening a file called Database with no filename extension as its unneeded 
+            try {
+                FileOutputStream fos = new FileOutputStream(file); // File Output Stream is used to write binary data from the hashmap 
+                ObjectOutputStream oos = new ObjectOutputStream(fos);   // Works on the bytes of the given object
+                oos.writeObject(profile); // Can write hashmap to the stream as it is serialilizable 
+                oos.flush(); // A buffer which allows all the bytes to be written while the program waits
+                oos.close();
+                fos.close(); // Closing frees up system resources
+            } catch (IOException i) { // IOExceptions are caused by errors in writing or reading files
+                System.out.println("Error writing");
+            }
+        } catch (Exception x) {
+            System.out.println("Error opening");
+        }
     }
 
 
