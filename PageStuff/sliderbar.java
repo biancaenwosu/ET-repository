ba815df564228c3789f4087c7871715b0f648463
Ezinwa <bianca.e.nwosu@gmail.com>
2024-09-17 23:03:03 +0100

    settings

---
+++ PageStuff/sliderbar.java
@@ -0,0 +1,35 @@
+
+import javax.swing.*;
+
+
+
+public class sliderbar {
+    JSlider slider;
+    int spacing;
+    public sliderbar(String average){
+        if ("0".equals(average)){
+            average = "500";
+        }
+        int maximum = Math.round(((Double.valueOf(average).intValue()*3)/50.0f))*50;
+        spacing = maximum/10;
+        System.out.println(spacing);
+        
+        slider = new JSlider(JSlider.HORIZONTAL,0,maximum,Double.valueOf(average).intValue());
+        //slider.setSize(300,4);
+        slider.setMinorTickSpacing(spacing);
+        slider.setMajorTickSpacing(spacing);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+    }
+    public int sliderListener(){
+        int SliderValue = slider.getValue();
+
+        return SliderValue;
+    }
+    
+    
+
+
+
+}
+
