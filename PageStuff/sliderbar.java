bd0b30040e6832bc16d6c248f13e4a3f3487c10d
Ezinwa <bianca.e.nwosu@gmail.com>
2024-09-18 19:33:58 +0100

    update settings

--- PageStuff/sliderbar.java
+++ PageStuff/sliderbar.java
@@ -1,10 +1,10 @@
-
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class sliderbar {
     JSlider slider;
+<<<<<<< Updated upstream
     int sliderValue;
     JFrame frame;
     public int slider(int max,int average,SliderValueListener listener){
@@ -30,6 +30,29 @@ public class sliderbar {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     
+=======
+    int spacing;
+    public sliderbar(String average){
+        if ("0".equals(average)){
+            average = "500";
+        }
+        int maximum = Math.round(((Double.valueOf(average).intValue()*3)/50.0f))*50;
+        spacing = maximum/10;
+        System.out.println(spacing);
+
+        slider = new JSlider(JSlider.HORIZONTAL,0,maximum,Double.valueOf(average).intValue());
+        //slider.setSize(300,4);
+        slider.setMinorTickSpacing(spacing);
+        slider.setMajorTickSpacing(spacing);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        }
+    public int sliderListener(){
+        int SliderValue = slider.getValue();
+
+        return SliderValue;
+        }
+>>>>>>> Stashed changes
 
         return sliderValue;
     }
@@ -50,6 +73,4 @@ public class sliderbar {
         System.out.println(x);
     }
 
-
 }
-
